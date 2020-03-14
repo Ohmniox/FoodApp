30c31b5ef697d40301a3f4f09eb2ffc476b6b594
Ohm Jodhani <ohm.jodhani@dynamicelements.no>
2020-03-14 11:12:16 +0530

    Added Data and Entities project. Added DbContext in ConfigureServices.

---
+++ FoodApp/FoodApp.Data/FoodAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FoodApp.Data
+{
+    public class FoodAppDbContext : DbContext
+    {
+        public FoodAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
