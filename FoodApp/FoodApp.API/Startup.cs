a5f053c3faab601cf490c5ed7355ed202e38ec1b
Ohm Jodhani <ohm.jodhani@dynamicelements.no>
2020-03-14 22:57:41 +0530

    Configured JsonOptions.

--- FoodApp/FoodApp.API/Startup.cs
+++ FoodApp/FoodApp.API/Startup.cs
@@ -29,7 +29,11 @@ namespace FoodApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            });
             
             var connectionString = Configuration.GetConnectionString("FoodAppDBConnectionString");
             services.AddDbContext<FoodAppDbContext>(item => item.UseSqlServer(connectionString));
